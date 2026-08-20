e73c00462928eaadad51a6d500f1b988f5efb3df
lucasxpaire <lxpaire@inf.ufsm.br>
2026-08-20 20:57:51 -0300

    Cria estrutura inicial e estiliza o cabecalho

---
+++ config.js
@@ -0,0 +1,2 @@
+export const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbytG3Orkk3LTU3l07X3zGXF0oU2XmCAsK2JtdKU6LKWA6rcZEfNfvoQSzABYuPm_vjm/exec";
+export const FRONTEND_VERSION = "1.1.0";
